22a12677b24db39aec26c8e1093261f2a8288900
exonti <me@exonti.dev>
2026-02-15 18:55:17 +0100

    working pda very happy

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,18 @@ use sips::{
     },
 };
 
-fn main() {}
+fn main() {
+    let program = Address::from_str_const("6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P");
+    // let shiet = sips::address::create_program_address(&[], &program).unwrap();
+
+    let mint = Address::from_str_const("Cf324EQSX3b2LdUyJ1aR11qiMzWNRsixG6G2pduLpump").to_bytes();
+    //let shiet2 = solana_pubkey::Pubkey::create_program_address(seeds, &program.into()).unwrap();
+
+    let seeds: &[&[u8]] = &[b"bonding-curve", mint.as_slice()];
+    let myaddress = Address::pda(&program, seeds);
+    let program = Address::from_str_const("6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P");
+    let address = solana_pubkey::Pubkey::find_program_address(seeds, &program.into());
+
+    dbg!(address.0);
+    dbg!(myaddress.0.to_bytes());
+}
